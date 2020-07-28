2fd36b4f6da691d7fb3888537863cc36ea7ba1af
LennardEc <58231666+LennardEc@users.noreply.github.com>
2020-07-28 12:24:16 +0200

    AGB Version Control v1

--- app/src/main/java/com/example/clicker1/AgbActivity.java
+++ app/src/main/java/com/example/clicker1/AgbActivity.java
@@ -24,7 +24,8 @@ public class AgbActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         email = intent.getStringExtra(MainActivity.EMAIL);
-        agb_version = intent.getIntExtra(MainActivity.AGB_VERSION, 1);
+
+        agb_version = MainActivity.AGB_VERSION;
 
         TextView agb_text = findViewById(R.id.agb_textView);
         agb_text.setMovementMethod(new ScrollingMovementMethod());
@@ -45,7 +46,7 @@ public class AgbActivity extends AppCompatActivity {
                     intent.putExtra(MainActivity.EMAIL, email);
                     startActivity(intent);
                 }else if(agb_check.isChecked() && isUser) {
-
+                    HelperFunctions.setAGBStatus(email, AgbActivity.this, agb_version);
 
                     Intent intent = new Intent(AgbActivity.this, AdActivity.class);
                     intent.putExtra(MainActivity.EMAIL, email);
