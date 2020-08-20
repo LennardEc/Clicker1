998ede33090f8003843840d89f40df157c68eca6
LennardEc <58231666+LennardEc@users.noreply.github.com>
2020-08-20 14:05:15 +0200

    Delete Warnings

--- app/src/main/java/com/example/clicker1/Utils.java
+++ app/src/main/java/com/example/clicker1/Utils.java
@@ -1,8 +1,8 @@
 package com.example.clicker1;
 
 public class Utils {
-    public static final String EMAIL_FROM = "getrichpayoutrequest@gmail.com";
-    public static final String PASSWORD_FROM = "clickgetRich";
+    public static final String EMAIL_FROM = "clickit.payoutrequest@gmail.com";
+    public static final String PASSWORD_FROM = "gTisT4320";
 
-    public static final String EMAIL_TO = "clickclickmoney57@gmail.com";
+    public static final String EMAIL_TO = "clickit.main@gmail.com";
 }
